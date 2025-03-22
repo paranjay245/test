aa4e647bf753394c4c3399a90a68f420c059f8df
paranjay245 <kparanjay245@gmail.com>
2025-03-22 08:18:08 +0530

    0.0.1

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VibeSite from './VibeSite'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VibeSite />
+  </React.StrictMode>
+)
